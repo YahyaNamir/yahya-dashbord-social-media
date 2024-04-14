b1408e58935987e92fdf6d2f874e30ef4de81f0c
Yahya Namir <yahyanamir5@gmail.com>
2024-04-14 16:44:36 +0100

    img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import '../styles/card.css'
+import React from "react";
+import "../styles/card.css";
 
-
-
-function Card({userName,followers,todayFollowers, icon, name}) {
-    const cardClass = `card ${name}`
-    return (
-        <article className={cardClass}>
-            <p className="card-title">
-                <img src= {icon}/>
-                {userName}
-            </p>
-            <p className="card-followers">
-            <span className="card-followers-number">{followers}</span>
-                <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-                <img src="images/up.png"/>
-                {todayFollowers} today
-            </p>
-        </article>
-    )
+function Card({ userName, followers, todayFollowers, icon, name }) {
+  const cardClass = `card ${name}`;
+  return (
+    <article className={cardClass}>
+      <p className="card-title">
+        <img alt="img" src={icon} />
+        {userName}
+      </p>
+      <p className="card-followers">
+        <span className="card-followers-number">{followers}</span>
+        <span className="card-followers-title">Followers</span>
+      </p>
+      <p className="card-today">
+        <img alt="img" src="images/up.png" />
+        {todayFollowers} today
+      </p>
+    </article>
+  );
 }
 
 export default Card;
